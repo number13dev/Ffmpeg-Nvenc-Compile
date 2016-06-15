96aaa265a2818a98f56f450a0389122d99b833c8
cooliobr <rafael@regs.com.br>
2016-06-15 09:38:20 -0300

    Update ffmpeg_nvenc.sh

--- ffmpeg_nvenc.sh
+++ ffmpeg_nvenc.sh
@@ -11,7 +11,7 @@ rm -rf ~/ffmpeg_build ~/ffmpeg_sources ~/bin/{ffmpeg,ffprobe,ffserver,vsyasm,x26
 installLibs(){
 echo "Installing prerequosites"
 sudo apt-get update
-sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
+sudo apt-get -y --force-yes install unzip autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
   libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
   libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev pulseaudio libpulse*
 }
