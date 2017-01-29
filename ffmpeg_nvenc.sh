73020efde16b366cfb3c21395f1d69b4893c0759
eclipse10000 <joerg.hetzner@fau.de>
2017-01-29 15:07:16 +0100

    Update ffmpeg_nvenc.sh

--- ffmpeg_nvenc.sh
+++ ffmpeg_nvenc.sh
@@ -147,7 +147,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libvorbis \
   --enable-libvpx \
   --enable-libx265 \
-  --enable-libx264 \
   --enable-nvenc \
   --enable-nonfree
 PATH="$HOME/bin:$PATH" make -j8
