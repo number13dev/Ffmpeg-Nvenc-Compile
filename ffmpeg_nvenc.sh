165d7e5e67c891c5cd1961defde3d01b92c6f1e0
eclipse10000 <joerg.hetzner@fau.de>
2017-01-29 14:25:29 +0100

    Update ffmpeg_nvenc.sh

--- ffmpeg_nvenc.sh
+++ ffmpeg_nvenc.sh
@@ -137,7 +137,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-cflags="-I$HOME/ffmpeg_build/include" \
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
   --bindir="$HOME/sbin" \
-  --enable-gpl \
   --enable-pthreads \
   --enable-libass \
   --enable-libfdk-aac \
