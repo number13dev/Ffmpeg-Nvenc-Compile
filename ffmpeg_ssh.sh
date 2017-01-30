d729120c5ce4ee6660bfbfcb89ed74c85c1186ea
eclipse10000 <joerg.hetzner@fau.de>
2017-01-30 15:13:08 +0100

    Update ffmpeg_ssh.sh

--- ffmpeg_ssh.sh
+++ ffmpeg_ssh.sh
@@ -22,7 +22,7 @@ cd libssh*
 mkdir build
 cd build
 pwd
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DOPENSSL_ROOT_DIR=/usr/local/ssl -DOPENSSL_LIBRARIES=/usr/lib/x86_64-linux-gnu/ -DENABLE_SHARED:bool=off ..
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DOPENSSL_LIBRARIES=/usr/lib/x86_64-linux-gnu/ -DENABLE_SHARED:bool=off ..
 PATH="$HOME/bin:$PATH" make
 make install
 make distclean
