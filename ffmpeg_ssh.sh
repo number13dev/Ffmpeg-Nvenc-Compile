987fbada35f41881ca0b5a85fb3e057af86f4c86
eclipse10000 <joerg.hetzner@fau.de>
2017-01-30 15:27:05 +0100

    Update ffmpeg_ssh.sh

--- ffmpeg_ssh.sh
+++ ffmpeg_ssh.sh
@@ -48,7 +48,7 @@ cd ~/ffmpeg_sources
 wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
 tar xjvf last_x264.tar.bz2
 cd x264-snapshot*
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --disable-shared
 PATH="$HOME/bin:$PATH" make
 make install
 make distclean
