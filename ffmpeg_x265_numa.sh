2719b2d487447572bb988e17387c0739645a794e
eclipse10000 <joerg.hetzner@fau.de>
2017-03-10 00:55:11 +0100

    Update ffmpeg_x265_numa.sh

--- ffmpeg_x265_numa.sh
+++ ffmpeg_x265_numa.sh
@@ -29,7 +29,7 @@ echo "Compiling libx265"
 cd ~/ffmpeg_sources
 hg clone https://bitbucket.org/multicoreware/x265
 cd ~/ffmpeg_sources/x265/build/linux
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" ../../source
 PATH="$HOME/bin:$PATH" make
 make install
 make distclean
