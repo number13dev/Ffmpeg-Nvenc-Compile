ff3c564fcb3fdc6a2f89bf71bb069235ca28b831
eclipse10000 <joerg.hetzner@fau.de>
2017-05-28 00:25:23 +0200

    Update ffmpeg_nvenc.sh

--- ffmpeg_nvenc.sh
+++ ffmpeg_nvenc.sh
@@ -8,7 +8,7 @@ installLibs(){
 echo "Installing prerequosites"
 sudo apt-get update
 sudo apt-get -y --force-yes install curl unzip cmake mercurial git autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
-  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+  libsdl1.2-dev libtool libva-dev libvdpau-dev libxcb1-dev libxcb-shm0-dev \
   libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev
 }
 
@@ -37,32 +37,6 @@ make install
 make distclean
 }
 
-#Compile libx264
-compileLibX264(){
-echo "Compiling libx264"
-cd ~/ffmpeg_sources
-wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
-tar xjvf last_x264.tar.bz2
-cd x264-snapshot*
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-PATH="$HOME/bin:$PATH" make
-make install
-make distclean
-}
-
-#Compile libx265
-compileLibX265(){
-echo "Compiling libx265"
-cd ~/ffmpeg_sources
-hg clone https://bitbucket.org/multicoreware/x265
-cd ~/ffmpeg_sources/x265/build/linux
-PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-PATH="$HOME/bin:$PATH" make
-make install
-make distclean
-}
-
-
 #Compile libfdk-acc
 compileLibfdkcc(){
 echo "Compiling libfdk-cc"
@@ -75,46 +49,6 @@ autoreconf -fiv
 make
 make install
 make distclean
-}
-
-#Compile libmp3lame
-compileLibMP3Lame(){
-echo "Compiling libmp3lame"
-#sudo apt-get install nasm
-cd ~/ffmpeg_sources
-curl http://sourceforge.mirrorservice.org/l/la/lame/lame/3.99/lame-3.99.5.tar.gz --retry 50 -O -L
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
-make
-make install
-make distclean
-}
-
-#Compile libopus
-compileLibOpus(){
-echo "Compiling libopus"
-cd ~/ffmpeg_sources
-wget http://downloads.xiph.org/releases/opus/opus-1.1.4.tar.gz
-tar xzvf opus-1.1.4.tar.gz
-cd opus-1.1.4
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
-}
-
-#Compile libvpx
-compileLibPvx(){
-echo "Compiling libvpx"
-cd ~/ffmpeg_sources
-wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.6.1.tar.bz2
-tar xjvf libvpx-1.6.1.tar.bz2
-cd libvpx-1.6.1
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
-PATH="$HOME/bin:$PATH" make
-make install
-make clean
 }
 
 #Compile ffmpeg
@@ -143,11 +77,6 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --enable-libass \
   --enable-libfdk-aac \
   --enable-libfreetype \
-  --enable-libmp3lame \
-  --enable-libopus \
-  --enable-libtheora \
-  --enable-libvorbis \
-  --enable-libvpx \
   --enable-nvenc \
   --enable-nonfree
 PATH="$HOME/bin:$PATH" make -j
@@ -162,11 +91,6 @@ mkdir ffmpeg_sources
 installLibs
 installSDK
 compileYasm
-#compileLibX264
-#compileLibX265
 compileLibfdkcc
-compileLibMP3Lame
-compileLibOpus
-compileLibPvx
 compileFfmpeg
 echo "Complete!"
